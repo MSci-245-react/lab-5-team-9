44cf5e5957f1381820a271c6d63890fbc8f01949
Rozalin Draghosian <rozalindraghosian@gmail.com>
2024-03-18 12:35:51 -0400

    finished lab 10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import RestaurantScreen from './components/RestaurantScreen';
+import {Provider} from 'react-redux';
+import store from './store';
 
- export default function App() {
-  return (
-    <div>
-      <RestaurantScreen />
-    </div>
-  );
- }
+export default function App() {
+   return (
+          <Provider store={store}>
+          <RestaurantScreen />
+          </Provider>
+    );
+  }
